37430cc456ad780c7e7794a73bb9ba7e5c40151b
prozolic <42107886+prozolic@users.noreply.github.com>
2025-10-12 21:58:18 +0900

    Add test

--- tests/GuidSupplement.Tests/UnitTests.cs
+++ tests/GuidSupplement.Tests/UnitTests.cs
@@ -1,4 +1,4 @@
-
+﻿
 using Xunit;
 using Shouldly;
 
@@ -11,7 +11,61 @@ public class GuidSupplementTest
     public void CreateTest()
     {
         var id = GuidVersion7.Create();
+
+        var timeStamp = DateTimeOffset.UtcNow;
+        var id2 = GuidVersion7.Create(timeStamp);
+    }
+
+    [Fact]
+    public void GetTimestampTest()
+    {
+        var expected = new DateTimeOffset(2025, 10, 12, 19, 56, 28, TimeSpan.Zero);
+        var id = GuidVersion7.Create(expected);
         var timestamp = GuidVersion7.GetTimestamp(id);
+
+        timestamp.ToUnixTimeMilliseconds().ShouldBe(expected.ToUnixTimeMilliseconds());
+    }
+
+    [Fact]
+    public void GetTimestampFailedTest()
+    {
+        Should.Throw<ArgumentException>(() => GuidVersion7.GetTimestamp(Guid.NewGuid()));
+    }
+
+    [Fact]
+    public void GetUnixTimeSecondsTest()
+    {
+        var expected = new DateTimeOffset(2025, 10, 12, 19, 56, 28, TimeSpan.Zero);
+        var id = GuidVersion7.Create(expected);
+        var timestamp = GuidVersion7.GetUnixTimeSeconds(id);
+
+        timestamp.ShouldBe(expected.ToUnixTimeMilliseconds());
+    }
+
+    [Fact]
+    public void GetUnixTimeSecondsFailedTest()
+    {
+        Should.Throw<ArgumentException>(() => GuidVersion7.GetUnixTimeSeconds(Guid.NewGuid()));
+    }
+
+    [Fact]
+    public void GetVersionTest()
+    {
+        var expected = 7;
+        var id = GuidVersion7.Create();
+        var version = GuidVersion7.GetVersion(id);
+        version.ShouldBe(expected);
+    }
+
+
+    [Fact]
+    public void IsVersion7Test()
+    {
+        var id = Guid.NewGuid();
+        GuidVersion7.IsVersion7(id).ShouldBeFalse();
+
+        var id2 = GuidVersion7.Create();
+        GuidVersion7.IsVersion7(id2).ShouldBeTrue();
     }
 
     [Fact]
